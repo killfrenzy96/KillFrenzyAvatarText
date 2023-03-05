b7ff7658b89735409e55335a6edb099789c268b1
killfrenzy96 <evan298@gmail.com>
2023-03-06 03:17:02 +1100

    convert license on files to MIT

--- Scripts/KAT_Main.cs
+++ Scripts/KAT_Main.cs
@@ -1,19 +1,22 @@
 /*
-	Avatar Text for VRChat
-	Copyright (C) 2022 KillFrenzy / Evan Tran
+Copyright 2023 KillFrenzy / Evan Tran
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-	You should have received a copy of the GNU General Public License
-	along with this program. If not, see <https://www.gnu.org/licenses/>.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #if UNITY_EDITOR
@@ -97,7 +100,7 @@ namespace KillFrenzy.AvatarTextTools
 				fontStyle = FontStyle.Bold
 			};
 
-			EditorGUILayout.LabelField("KillFrenzy Avatar Text (KAT) v1.3.0", titleStyle);
+			EditorGUILayout.LabelField("KillFrenzy Avatar Text (KAT) v1.3.2", titleStyle);
 
 			// Tab to switch between simple installer or modular installer
 			#if VRC_SDK_VRCSDK3 && CVR_CCK_EXISTS
